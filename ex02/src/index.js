cc89ff3220442dbdfe88a546df5643fd353c0aa0
AhmedMehmedovic <ahmedm.bjelave@gmail.com>
2021-09-15 19:25:28 +0200

    Exercise two

---
+++ ex02/src/index.js
@@ -0,0 +1,26 @@
+function main () {
+
+    // Only change code below this line
+    
+// Ovo je jednoredni Javascript komentar
+
+
+   /* 
+    document.writer ("This is a multi-line comment!");
+    document.writer ("This is a multi-line comment!");
+    document.writer ("This is a multi-line comment!");
+    document.writer ("This is a multi-line comment!");
+    document.writer ("This is a multi-line comment!");
+    document.writer ("This is a multi-line comment!");
+
+    */
+
+
+
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
